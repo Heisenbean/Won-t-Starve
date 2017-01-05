43a01e7e1d3cde8165a95c84ca1e1071c89e5db5
Heisenbean <hedongbin1991@gmail.com>
2017-01-05 15:00:05 +0800

    init code

---
+++ Won't Starve/Won't Starve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Won't Starve
+//
+//  Created by Heisenbean on 17/1/5.
+//  Copyright © 2017年 Heisenbean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
